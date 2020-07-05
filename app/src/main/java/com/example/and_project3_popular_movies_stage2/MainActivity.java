83937fd1f8e3d3e57c30c6855380f8c253175873
brkline <brkline@users.noreply.github.com>
2020-07-05 07:25:07 -0400

    Removed SharedPreferences and started wiring up menu.

--- app/src/main/java/com/example/and_project3_popular_movies_stage2/MainActivity.java
+++ app/src/main/java/com/example/and_project3_popular_movies_stage2/MainActivity.java
@@ -28,7 +28,7 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Movie>>, SharedPreferences.OnSharedPreferenceChangeListener {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Movie>> {
 
     // Constant value for the movie item loader ID.
     private static final int MOVIE_LOADER_ID = 1;
@@ -54,7 +54,29 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 //        emptyStateTextView = findViewById(R.id.empty_view);
+        loadMovies();
 
+        // sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+//        setupSharedPreferences();
+        // Get a reference to the GridView
+//        gridView = findViewById(R.id.main_fragment_gv);
+//        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                launchMovieDetailActivity(position);
+//            }
+//        });
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                launchMovieDetailActivity(position);
+            }
+        });
+    }
+
+    private void loadMovies() {
         // CheckIfOnline class based on https://stackoverflow.com/a/27312494
         new CheckIfOnline(new CheckIfOnline.Consumer() {
             // Anonymous class created using https://stackoverflow.com/a/40826752
@@ -75,25 +97,6 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 //                    emptyStateTextView.setText(R.string.no_internet_connection);
                     emptyStateTextView.setText(R.string.no_internet_connection);
                 }
-            }
-        });
-
-        // sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-        setupSharedPreferences();
-        // Get a reference to the GridView
-//        gridView = findViewById(R.id.main_fragment_gv);
-//        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                launchMovieDetailActivity(position);
-//            }
-//        });
-
-        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                launchMovieDetailActivity(position);
             }
         });
     }
@@ -132,19 +135,44 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     // This method initialize the contents of the Activity's options menu.
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the Options Menu we specified in XML
-        getMenuInflater().inflate(R.menu.menu_settings, menu);
-        return true;
+//        getMenuInflater().inflate(R.menu.menu_settings, menu);
+        getMenuInflater().inflate(R.menu.primary_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+//        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            Intent settingsIntent = new Intent(this, MovieSettingsActivity.class);
-            startActivity(settingsIntent);
-            return true;
+//        int id = item.getItemId();
+//        if (id == R.id.action_settings) {
+//            Intent settingsIntent = new Intent(this, MovieSettingsActivity.class);
+//            startActivity(settingsIntent);
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+        int idOfMenuItemSelected = item.getItemId();
+        switch (idOfMenuItemSelected) {
+            case R.id.menu_popular:
+//                mostPopularSelected = true;
+//                topRatedSelected = false;
+                setTitle(R.string.primary_menu_most_popular_title);
+//                queryType = POPULAR;
+//                loadMovies(queryType);
+                break;
+            case R.id.menu_top_rated:
+//                mostPopularSelected = false;
+//                topRatedSelected = true;
+                setTitle(R.string.primary_menu_top_rated_title);
+//                queryType = TOP_RATED;
+//                loadMovies(queryType);
+                break;
+            case R.id.menu_favorites:
+//                mostPopularSelected = false;
+//                topRatedSelected = false;
+//                queryType = FAVORITES;
+                break;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
@@ -157,29 +185,29 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         startActivity(intent);
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        // Unregister VisualizerActivity as an OnPreferenceChangedListener to avoid any memory leaks.
-        PreferenceManager.getDefaultSharedPreferences(this)
-                .unregisterOnSharedPreferenceChangeListener(this);
-    }
-
-    private void setupSharedPreferences() {
-        // Get all of the values from shared preferences to set it up
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        String sortOrderSelected = sharedPreferences.getString(getString(R.string
-                .movie_settings_sort_order_key), getString(R.string.movie_settings_sort_label1));
-        setTitle(sortOrderSelected);
-        // Register the listener
-        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
-    }
-
-    @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        String sortOrderSelected = sharedPreferences.getString(getString(R.string
-                .movie_settings_sort_order_key), getString(R.string.movie_settings_sort_default));
-        setTitle(sortOrderSelected);
-    }
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//        // Unregister VisualizerActivity as an OnPreferenceChangedListener to avoid any memory leaks.
+//        PreferenceManager.getDefaultSharedPreferences(this)
+//                .unregisterOnSharedPreferenceChangeListener(this);
+//    }
+//
+//    private void setupSharedPreferences() {
+//        // Get all of the values from shared preferences to set it up
+//        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+//        String sortOrderSelected = sharedPreferences.getString(getString(R.string
+//                .movie_settings_sort_order_key), getString(R.string.movie_settings_sort_label1));
+//        setTitle(sortOrderSelected);
+//        // Register the listener
+//        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+//    }
+//
+//    @Override
+//    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+//        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+//        String sortOrderSelected = sharedPreferences.getString(getString(R.string
+//                .movie_settings_sort_order_key), getString(R.string.movie_settings_sort_default));
+//        setTitle(sortOrderSelected);
+//    }
 }
